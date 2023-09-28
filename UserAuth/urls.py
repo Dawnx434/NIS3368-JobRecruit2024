af456b09f6b62406a127d230e62f4c0ef8191aa7
glimmeringlight <3137181992@qq.com>
2023-09-28 16:06:42 +0800

    Fix some problems

--- UserAuth/urls.py
+++ UserAuth/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path("gencode/", views.generate_verification_code),
     path("state/", views.checkLoginState),
     path("logout/", views.logout),
-    path('', views.index),
 ]
