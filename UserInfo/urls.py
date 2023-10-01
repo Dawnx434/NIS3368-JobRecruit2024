d3ba8396b2dda8518536c1e6cc92ef4424b0dc18
glimmeringlight <3137181992@qq.com>
2023-10-02 00:14:14 +0800

    Optimize: code about modify user info

--- UserInfo/urls.py
+++ UserInfo/urls.py
@@ -2,12 +2,10 @@ from django.urls import path
 from UserInfo import views
 
 urlpatterns = [
-   path("index/", views.index),
-   path("resume/", views.resume),
-   path("application/", views.apply),
-   path("info/", views.info),
-   path("account/",views.account),
-   path("logout/",views.logout),
-   path("modify/", views.modify),
-   path("submit/",views.change_sql),
+    path("index/", views.index),
+    path("resume/", views.resume),
+    path("application/", views.apply),
+    path("account/", views.account),
+    path("modify/", views.modify),
+    path("submit/", views.change_sql),
 ]
