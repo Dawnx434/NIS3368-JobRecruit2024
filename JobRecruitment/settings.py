e37db695ab8229512e0f1327ea15d6240a2c9521
cyny666 <1468578931@qq.com>
2023-10-17 09:32:00 +0800

    modify some

--- JobRecruitment/settings.py
+++ JobRecruitment/settings.py
@@ -131,7 +131,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images/')
-RESUME_ROOT = os.path.join(BASE_DIR, 'static/resumes/')
+RESUME_ROOT = os.path.join(BASE_DIR, 'resumes/')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
