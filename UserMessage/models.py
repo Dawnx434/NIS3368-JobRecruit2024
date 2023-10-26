571fb9d7b2595e033b83cfecdff855e089e3c1a4
glimmeringlight <3137181992@qq.com>
2023-10-26 14:40:16 +0800

    Finish module Message

--- UserMessage/models.py
+++ UserMessage/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 
 from UserAuth.models import User
@@ -9,6 +11,16 @@ class Message(models.Model):
                                   related_name="Message_from_user")
     to_user = models.ForeignKey(verbose_name="接收方", to=User, to_field='id', on_delete=models.CASCADE,
                                 related_name="Message_to_user")
-    title = models.CharField(verbose_name="标题", max_length=32)
+    title = models.CharField(verbose_name="标题", max_length=100)
     content = models.TextField(verbose_name="内容", max_length=2000)
     create_time = models.DateTimeField(verbose_name='私信发送时间', auto_now_add=False)
+    read_choice = (
+        (0, "未读"),
+        (1, "已读")
+    )
+    read = models.SmallIntegerField(verbose_name="私信状态", choices=read_choice, default=0)
+    reply_to = models.ForeignKey(verbose_name="回复至", to='self', on_delete=models.CASCADE, null=True, blank=True)
+
+    def __str__(self):
+        return "{}于{}发往{}的私信".format(self.from_user, (self.create_time + datetime.timedelta(hours=8)).strftime(
+            "%Y-%m-%d %H:%M:%S"), self.to_user)
--- UserMessage/urls.py
+++ UserMessage/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from UserMessage import views
 
+app_name = "UserMessage"
 urlpatterns = [
     path("send/", views.send_message),
+    path("list/", views.message_list, name="message_box"),
+    path("view/<int:mid>/", views.view_message_detail)
 ]
