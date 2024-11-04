7a2285a89210ad4ea55a71972fc34ed1f73c0397
Junxiao Kong | 孔珺晓 <1980188239@qq.com>
2024-11-04 11:45:32 +0800

    update setting

--- JobRecruitment/settings.py
+++ JobRecruitment/settings.py
@@ -142,9 +142,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/plugins'),  # 插件目录
+    os.path.join(BASE_DIR, 'static/js'),  # JS 目录
+    os.path.join(BASE_DIR, 'static/img'),  # 图片目录
+]
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
 
